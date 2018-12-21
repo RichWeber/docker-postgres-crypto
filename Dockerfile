4531ffb3e87bb460d60c7bd977c6e93b8a6ce55b
RichWeber <rbagatyi@gmail.com>
2018-12-21 20:34:36 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM postgres:10.1
+FROM postgres:11
 
 RUN apt update && apt -y install postgresql-contrib
 COPY init-pgcrypto.sh /docker-entrypoint-initdb.d/
--- init-pgcrypto.sh
+++ init-pgcrypto.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE EXTENSION pgcrypto; CREATE EXTENSION \"uuid-ossp\";"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE EXTENSION pgcrypto;"
